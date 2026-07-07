694c0b8eb0f87ce99d15b2b132009c80d17e6957
ahmedidrees463 <ahmedidrees1243@gmail.com>
2026-07-07 19:15:53 +0800

    Lesson 2

---
+++ Activity 1.py
@@ -0,0 +1,29 @@
+snack_name="Chocolate"
+price=1.50
+quantity=10
+is_availble=True
+
+print("snack:",snack_name)
+print("price:",price)
+print("quanity:",quantity)
+print("is_availble:",is_availble)
+
+total=price*quantity
+print("total value:",total)
+print("sale price:",price-0.25)
+print("double stock:",quantity*2)
+
+print("is price under $ 2:",price<2)
+print("more than 5 in stock:",quantity>5)
+print("is price exactly same 1.50",price==1.50)
+
+shop="Quick"+" "+"wipes"
+print("shop:",shop)
+print("Letters in shop:",len(shop))
+print("Frist letter:",shop[0])
+
+temp=price
+price=quantity
+quantity=temp
+print(price)
+print(quantity)
